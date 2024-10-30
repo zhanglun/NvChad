092bfdd11b1322bb39fc07f695ccb95d352be3fb
zhanglun <zhanglun1410@gmail.com>
2024-10-30 17:49:37 +0800

    feat: update dashboard

--- lua/nvchad/configs/alpha.lua
+++ lua/nvchad/configs/alpha.lua
@@ -1,110 +1,66 @@
-local present, alpha = pcall(require, "alpha")
+local alpha = require("alpha")
+local dashboard = require("alpha.themes.dashboard")
+local logo = [[
+  ██╗      █████╗ ███████╗██╗   ██╗██╗   ██╗██╗███╗   ███╗          Z
+  ██║     ██╔══██╗╚══███╔╝╚██╗ ██╔╝██║   ██║██║████╗ ████║      Z
+  ██║     ███████║  ███╔╝  ╚████╔╝ ██║   ██║██║██╔████╔██║   z
+  ██║     ██╔══██║ ███╔╝    ╚██╔╝  ╚██╗ ██╔╝██║██║╚██╔╝██║ z
+  ███████╗██║  ██║███████╗   ██║    ╚████╔╝ ██║██║ ╚═╝ ██║
+  ╚══════╝╚═╝  ╚═╝╚══════╝   ╚═╝     ╚═══╝  ╚═╝╚═╝     ╚═╝
+]]
 
-if not present then
-  return
-end
 
--- require("base46").load_all_highlight "alpha"
-
-local function button(sc, txt, keybind)
-  local sc_ = sc:gsub("%s", ""):gsub("SPC", "<leader>")
-
-  local opts = {
-    position = "center",
-    text = txt,
-    shortcut = sc,
-    cursor = 5,
-    width = 36,
-    align_shortcut = "right",
-    hl = "AlphaButton",
-  }
-
-  if keybind then
-    opts.keymap = { "n", sc_, keybind, { noremap = true, silent = true } }
-  end
-
-  return {
-    type = "button",
-    val = txt,
-    on_press = function()
-      local key = vim.api.nvim_replace_termcodes(sc_, true, false, true) or ""
-      vim.api.nvim_feedkeys(key, "normal", false)
-    end,
-    opts = opts,
-  }
-end
-
--- dynamic header padding
-local fn = vim.fn
-local marginTopPercent = 0.3
-local headerPadding = fn.max { 2, fn.floor(fn.winheight(0) * marginTopPercent) }
-
-local options = {
-
-  header = {
-    type = "text",
-    val = {
-      -- "                                                     ",
-      -- "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
-      -- "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
-      -- "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
-      -- "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
-      -- "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
-      -- "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
-      "                                                     ",
-      " █████╗ ███╗   ██╗ ██████╗ ████████╗██╗  ██╗███████╗██████╗      ██████╗ ██████╗ ███████╗ █████╗ ████████╗    ██████╗  █████╗ ██╗   ██╗ ",
-      " ██╔══██╗████╗  ██║██╔═══██╗╚══██╔══╝██║  ██║██╔════╝██╔══██╗    ██╔════╝ ██╔══██╗██╔════╝██╔══██╗╚══██╔══╝    ██╔══██╗██╔══██╗╚██╗ ██╔╝ ",
-      " ███████║██╔██╗ ██║██║   ██║   ██║   ███████║█████╗  ██████╔╝    ██║  ███╗██████╔╝█████╗  ███████║   ██║       ██║  ██║███████║ ╚████╔╝  ",
-      " ██╔══██║██║╚██╗██║██║   ██║   ██║   ██╔══██║██╔══╝  ██╔══██╗    ██║   ██║██╔══██╗██╔══╝  ██╔══██║   ██║       ██║  ██║██╔══██║  ╚██╔╝   ",
-      " ██║  ██║██║ ╚████║╚██████╔╝   ██║   ██║  ██║███████╗██║  ██║    ╚██████╔╝██║  ██║███████╗██║  ██║   ██║       ██████╔╝██║  ██║   ██║    ",
-      " ╚═╝  ╚═╝╚═╝  ╚═══╝ ╚═════╝    ╚═╝   ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝     ╚═════╝ ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝   ╚═╝       ╚═════╝ ╚═╝  ╚═╝   ╚═╝    ",
-    },
-    opts = {
-      position = "center",
-      hl = "AlphaHeader",
-    },
-  },
-
-  buttons = {
-    type = "group",
-    val = {
-      button("SPC f f", "  Find File  ", ":Telescope find_files<CR>"),
-      button("SPC f o", "  Recent File  ", ":Telescope oldfiles<CR>"),
-      button("SPC f w", "  Find Word  ", ":Telescope live_grep<CR>"),
-      button("SPC b m", "  Bookmarks  ", ":Telescope marks<CR>"),
-      button("SPC t h", "  Themes  ", ":Telescope themes<CR>"),
-      button("SPC c h", "  Keymaps  ", ":Telescope keymaps<CR>"),
-      button("SPC e s", "  Settings", ":e $MYVIMRC | :cd %:p:h <CR>"),
-      -- { txt = "─", hl = "NvDashLazy", no_gap = true, rep = true },
-
-      {
-        txt = function()
-          local stats = require("lazy").stats()
-          local ms = math.floor(stats.startuptime) .. " ms"
-          return "  Loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms
-        end,
-        -- hl = "NvDashLazy",
-        no_gap = true,
-      },
-
-      -- { txt = "─", hl = "NvDashLazy", no_gap = true, rep = true },
-    },
-    opts = {
-      spacing = 1,
-    },
-  },
-
-  headerPaddingTop = { type = "padding", val = headerPadding },
-  headerPaddingBottom = { type = "padding", val = 2 },
+-- Set menu
+dashboard.section.buttons.val = {
+  dashboard.button("e", "  > New file", ":ene <BAR> startinsert <CR>"),
+  dashboard.button("f", "  > Find file", ":cd $HOME/Workspace | Telescope find_files<CR>"),
+  dashboard.button("r", "  > Recent", ":Telescope oldfiles<CR>"),
+  dashboard.button("s", "  > Settings", ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
+  dashboard.button("q", "  > Quit NVIM", ":qa<CR>"),
+}
+dashboard.section.header.val = vim.split(logo, "\n")
+-- stylua: ignore
+dashboard.section.buttons.val = {
+  dashboard.button("SPC f f", " " .. " Find file", ":Telescope find_files<CR>"),
+  dashboard.button("SPC f n", " " .. " New file", [[<cmd> ene <BAR> startinsert <cr>]]),
+  dashboard.button("SPC f o", " " .. " Recent files", ":Telescope oldfiles<CR>"),
+  dashboard.button("SPC f w", " " .. " Find word", ":Telescope live_grep<CR>"),
+  dashboard.button("SPC f s", " " .. " Restore Session", [[<cmd> lua require("persistence").load() <cr>]]),
+  dashboard.button("SPC c h", " " .. " Cheatsheet  ", ":NvCheatsheet<CR>"),
+  dashboard.button("SPC t h", " " .. " Themes  ", ":Telescope themes<CR>"),
+  dashboard.button("SPC e s", " " .. " Settings", ":e $MYVIMRC | :cd %:p:h <CR>"),
+  dashboard.button("q", " " .. " Quit", "<cmd> qa <cr>"),
 }
 
+dashboard.opts.layout[1].val = 10
 
-alpha.setup {
-  layout = {
-    options.headerPaddingTop,
-    options.header,
-    options.headerPaddingBottom,
-    options.buttons,
-  },
-  opts = {},
-}
+-- Send config to alpha
+alpha.setup(dashboard.opts)
+
+-- Disable folding on alpha buffer
+vim.cmd([[
+    autocmd FileType alpha setlocal nofoldenable
+]])
+
+vim.api.nvim_create_autocmd("User", {
+  once = true,
+  pattern = "LazyVimStarted",
+  callback = function()
+    local stats = require("lazy").stats()
+    local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+    local info = {}
+    local fortune = require("fortune").get_fortune()
+    info[1] = "           ⚡ Neovim loaded "
+        .. stats.loaded
+        .. "/"
+        .. stats.count
+        .. " plugins in "
+        .. ms
+        .. "ms"
+
+    info[2] = " "
+    local footer = vim.list_extend(info, fortune)
+    dashboard.section.footer.val = footer
+    pcall(vim.cmd.AlphaRedraw)
+  end,
+})
--- lua/nvchad/plugins/init.lua
+++ lua/nvchad/plugins/init.lua
@@ -172,87 +172,17 @@ return {
       require("nvim-treesitter.configs").setup(opts)
     end,
   },
-  -- {
-  --   "goolord/alpha-nvim",
-  --   event = "VimEnter",
-  --   enabled = true,
-  --   init = false,
-  --   dependencies = { 'nvim-tree/nvim-web-devicons' },
-  --   config = function()
-  --     require("nvchad.configs.alpha")
-  --   end,
-  -- },
+  {
+    "rubiin/fortune.nvim"
+  },
   {
     "goolord/alpha-nvim",
     event = "VimEnter",
     enabled = true,
     init = false,
-    opts = function()
-      local dashboard = require("alpha.themes.dashboard")
-      local logo = [[
-           ██╗      █████╗ ███████╗██╗   ██╗██╗   ██╗██╗███╗   ███╗          Z
-           ██║     ██╔══██╗╚══███╔╝╚██╗ ██╔╝██║   ██║██║████╗ ████║      Z
-           ██║     ███████║  ███╔╝  ╚████╔╝ ██║   ██║██║██╔████╔██║   z
-           ██║     ██╔══██║ ███╔╝    ╚██╔╝  ╚██╗ ██╔╝██║██║╚██╔╝██║ z
-           ███████╗██║  ██║███████╗   ██║    ╚████╔╝ ██║██║ ╚═╝ ██║
-           ╚══════╝╚═╝  ╚═╝╚══════╝   ╚═╝     ╚═══╝  ╚═╝╚═╝     ╚═╝
-      ]]
-
-      dashboard.section.header.val = vim.split(logo, "\n")
-      -- stylua: ignore
-      dashboard.section.buttons.val = {
-        dashboard.button("SPC f f", " " .. " Find file", ":Telescope find_files<CR>"),
-        dashboard.button("SPC f n", " " .. " New file", [[<cmd> ene <BAR> startinsert <cr>]]),
-        dashboard.button("SPC f o", " " .. " Recent files", ":Telescope oldfiles<CR>"),
-        dashboard.button("SPC f w", " " .. " Find word", ":Telescope live_grep<CR>"),
-        -- dashboard.button("c", " " .. " Config", LazyVim.pick.config_files()),
-        dashboard.button("SPC f s", " " .. " Restore Session", [[<cmd> lua require("persistence").load() <cr>]]),
-        dashboard.button("SPC c h", " " .. " Cheatsheet  ", ":NvCheatsheet<CR>"),
-        dashboard.button("SPC t h", " " .. " Themes  ", ":Telescope themes<CR>"),
-        dashboard.button("SPC e s", " " .. " Settings", ":e $MYVIMRC | :cd %:p:h <CR>"),
-        dashboard.button("q", " " .. " Quit", "<cmd> qa <cr>"),
-      }
-      for _, button in ipairs(dashboard.section.buttons.val) do
-        button.opts.hl = "AlphaButton"
-        button.opts.hl_shortcut = "AlphaButtonShortcut"
-      end
-      dashboard.section.header.opts.hl = "AlphaHeader"
-      dashboard.section.buttons.opts.hl = "AlphaButton"
-      dashboard.section.footer.opts.hl = "AlphaFooter"
-      dashboard.opts.layout[1].val = 8
-      return dashboard
-    end,
-    config = function(_, dashboard)
-      -- close Lazy and re-open when the dashboard is ready
-      if vim.o.filetype == "lazy" then
-        vim.cmd.close()
-        vim.api.nvim_create_autocmd("User", {
-          once = true,
-          pattern = "AlphaReady",
-          callback = function()
-            require("lazy").show()
-          end,
-        })
-      end
-
-      require("alpha").setup(dashboard.opts)
-
-      vim.api.nvim_create_autocmd("User", {
-        once = true,
-        pattern = "LazyVimStarted",
-        callback = function()
-          local stats = require("lazy").stats()
-          local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
-          dashboard.section.footer.val = "⚡ Neovim loaded "
-              .. stats.loaded
-              .. "/"
-              .. stats.count
-              .. " plugins in "
-              .. ms
-              .. "ms"
-          pcall(vim.cmd.AlphaRedraw)
-        end,
-      })
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require("nvchad.configs.alpha")
     end,
   },
   {
